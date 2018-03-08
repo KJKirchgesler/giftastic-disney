38c02150a4bb582073090928656955da0086b9e0
KRISTEN KIRCHGESLER <kristen@KRISTENs-MacBook-Pro.local>
2018-03-08 11:36:59 -0500

    added code to index.html and pseudocode to giftastic.js

---
+++ assets/javascript/giftastic.js
@@ -0,0 +1,18 @@
+//Pseudocode
+//generate dynamic buttons from js script
+//list of buttons are populated from an array
+//loop over array to create dynamic buttons
+	//each button queries GIPHY API of a particular Disney character
+	//clicking on a button to pull 10 images from Giphy API	
+	//initally loaded gifs are still
+	//Each gif has a rating
+	//Click on a gif to make it animate
+	//Click again to make image still
+//Create a form that inputs text representing a Disney character
+//Submitting a value will:
+	//generate a dynamic button
+	//query the Giphy API for that character
+	//pull 10 still gifs of that character
+	//each gif has a rating
+	//clicking on a gif will animate it
+	//clicking it again will make it still
